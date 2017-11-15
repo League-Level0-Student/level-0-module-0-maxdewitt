e9476d444f97d7efd2ca818e4a2009081ddbe21a
Dewitt.Max <Dewitt.Max@SEMS-259-10.SEMS259.LAB>
2017-11-15 14:52:38 -0800

    I learned how to ask questions and display answers.

--- src/Greeter.java
+++ src/Greeter.java
@@ -9,6 +9,6 @@ String age = JOptionPane.showInputDialog("Type in your age");
 JOptionPane.showMessageDialog(null, "So you are " + age + " years old!"  );
 String hobby = JOptionPane.showInputDialog("Type in your hobby");
 JOptionPane.showMessageDialog(null, "So you like to (do) " + hobby + "!");
-JOptionPane.showMessageDialog(null, name + " is" age + " and likes to (do)" + hobby);
+JOptionPane.showMessageDialog(null, "name + " is " age + " and likes to " + hobby);
 }
 }

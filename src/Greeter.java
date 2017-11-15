dd5f6a574208dcdcc7cc9dd91e48bb3e3f5a125d
Dewitt.Max <Dewitt.Max@SEMS-259-10.SEMS259.LAB>
2017-11-15 14:51:57 -0800

    I learned how to ask and display answers using variables.

---
+++ src/Greeter.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Greeter {
+public static void main(String[] args) {
+	
+String name = JOptionPane.showInputDialog("Type in your name");
+JOptionPane.showMessageDialog(null, "Hello " + name + "!");
+String age = JOptionPane.showInputDialog("Type in your age");
+JOptionPane.showMessageDialog(null, "So you are " + age + " years old!"  );
+String hobby = JOptionPane.showInputDialog("Type in your hobby");
+JOptionPane.showMessageDialog(null, "So you like to (do) " + hobby + "!");
+JOptionPane.showMessageDialog(null, name + " is" age + " and likes to (do)" + hobby);
+}
+}
